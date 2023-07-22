31a910baf923a600eabd65285ff03cef2f465b49
rololevy <84113089+rololevy@users.noreply.github.com>
2023-07-21 22:18:13 -0300

    Create challenge_create_table.sql

---
+++ challenge_create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS rick_and_morty_final (
+    id INT,
+    name VARCHAR(255),
+    status VARCHAR(50),
+    species VARCHAR(100),
+    type VARCHAR(100),
+    gender VARCHAR(50),
+    origin VARCHAR(255),
+    location VARCHAR(255),
+    image VARCHAR(255),
+    created VARCHAR(50)
+);
