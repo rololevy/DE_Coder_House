b5b1577755d9356511b18fec6ec614669208aa9d
rololevy <84113089+rololevy@users.noreply.github.com>
2023-08-01 23:32:43 -0300

    Update Challenge_1.py

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,7 +1,9 @@
-pip install pandas
-pip install requests
 import pandas as pd
 import requests
+import time
+import json
+import psycopg2
+from sqlalchemy import create_engine
 
 # Realizar la solicitud GET a la API para obtener la lista de personajes
 url = 'https://rickandmortyapi.com/api/character'
@@ -18,23 +20,54 @@ if response.status_code == 200:
     # Crear una lista para almacenar la información detallada de cada personaje
     lista_detalles_personajes = []
 
-    # Iterar sobre la lista de personajes y obtener información detallada de cada uno
+    # Simular una limpieza de datos y agregar el timestamp a cada personaje
+    current_timestamp = int(time.time())  # Obtener el timestamp actual en segundos
     for personaje in personajes:
-        url_personaje = personaje['url']
-        response_personaje = requests.get(url_personaje)
-        if response_personaje.status_code == 200:
-            detalle_personaje = response_personaje.json()
-            lista_detalles_personajes.append(detalle_personaje)
-        else:
-            print('Error en la solicitud del personaje:', personaje['name'])
+        # Simulación de limpieza de datos
+        personaje['species'] = personaje['species'].replace('Unknown', 'N/A')
+        personaje['type'] = personaje['type'].replace('Unknown', 'N/A')
+        # Convertir diccionarios a strings en las columnas 'origin' y 'location'
+    
+        # Agregar el timestamp al personaje
+        personaje['timestamp'] = current_timestamp
+
+        # Agregar el personaje a la lista
+        lista_detalles_personajes.append(personaje)
 
     # Crear un DataFrame de Pandas a partir de la lista de diccionarios con los detalles de los personajes
     df = pd.DataFrame.from_records(lista_detalles_personajes)
+
+    # Mostrar las columnas del DataFrame
+    print(df.columns)
+
+    df['origin'] = df['origin'].apply(json.dumps)
+    df['location'] = df['location'].apply(json.dumps)
+    df['origin'] = df['origin'].astype(str)
+    df['location'] = df['location'].astype(str)
 
     # Mostrar el DataFrame
     print(df)
 
     # Guardar el DataFrame en un archivo CSV
     df.to_csv('rick_and_morty_characters.csv', index=False)
+    
+    # Insertar los datos en la base de datos (utilizando Pandas y SQLAlchemy)
+
+    connection_params = {
+        "dbname": " ",
+        "user": "",
+        "password": "",
+        "host": "",
+        "port": ""
+    }
+
+    engine = create_engine(
+        f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['dbname']}",
+        connect_args={"sslmode": "require"}
+    )
+    
+    df.to_sql('rick_and_morty_v3', engine, if_exists='append', index=False)
+
+    print("Datos insertados exitosamente en la tabla.")
 else:
     print('Error en la solicitud. Código de estado:', response.status_code)
