2cd8f3572e66a963dcf670822ff1ff3a38183425
rololevy <84113089+rololevy@users.noreply.github.com>
2023-08-01 23:31:09 -0300

    Update challenge_create_table.sql

--- challenge_create_table.sql
+++ challenge_create_table.sql
@@ -1,12 +1,15 @@
-CREATE TABLE IF NOT EXISTS rick_and_morty_final (
+CREATE TABLE rick_and_morty_v3 (
     id INT,
-    name VARCHAR(255),
+    name VARCHAR(100),
     status VARCHAR(50),
-    species VARCHAR(100),
-    type VARCHAR(100),
-    gender VARCHAR(50),
-    origin VARCHAR(255),
-    location VARCHAR(255),
-    image VARCHAR(255),
-    created VARCHAR(50)
+    species VARCHAR(50),
+    type VARCHAR(50),
+    gender VARCHAR(20),
+    origin VARCHAR(500),
+    location VARCHAR(500),
+    image VARCHAR(500),
+    episode VARCHAR(500),
+    url VARCHAR(500),
+    created TIMESTAMP,
+    timestamp BIGINT
 );
