7174bf69b5da21e082a25f43cbfcd08e6d57f97d
rololevy <84113089+rololevy@users.noreply.github.com>
2023-08-10 22:52:49 -0300

    Update challenge_create_table.sql

--- challenge_create_table.sql
+++ challenge_create_table.sql
@@ -1,15 +1,12 @@
-CREATE TABLE rick_and_morty_v3 (
-    id INT,
-    name VARCHAR(100),
-    status VARCHAR(50),
-    species VARCHAR(50),
-    type VARCHAR(50),
-    gender VARCHAR(20),
-    origin VARCHAR(500),
-    location VARCHAR(500),
-    image VARCHAR(500),
-    episode VARCHAR(500),
-    url VARCHAR(500),
-    created TIMESTAMP,
-    timestamp BIGINT
+CREATE TABLE rick_and_morty_v6
+(
+	id INTEGER NOT NULL ENCODE az64,
+	name VARCHAR(100) ENCODE lzo,
+	status VARCHAR(50) ENCODE lzo,
+	species VARCHAR(50) ENCODE lzo,
+	"type" VARCHAR(50) ENCODE lzo,
+	gender VARCHAR(20) ENCODE lzo,
+	created TIMESTAMP WITHOUT TIME ZONE ENCODE az64,
+	"timestamp" TIMESTAMP WITHOUT TIME ZONE ENCODE az64,
+	PRIMARY KEY (id)
 );
