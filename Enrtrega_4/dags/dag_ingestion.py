684e67369af285192c8e4c7a9f055d80b75948af
rololevy <84113089+rololevy@users.noreply.github.com>
2023-09-08 22:11:55 -0300

    Update dag_ingestion.py

--- Enrtrega_4/dags/dag_ingestion.py
+++ Enrtrega_4/dags/dag_ingestion.py
@@ -5,6 +5,30 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from desafio_2_v2 import main # Aqui se importan las funciones a ejecutar (las del desafio 2)
 from airflow.operators.email_operator import EmailOperator
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email():
+    subject = "Email Subject"
+    body = "This is the body of the text message"
+    sender = "o.oaguilera@gmail.com"
+    recipients = ["o.oaguilera@gmail.com", "o.oaguilera2@gmail.com"]
+    password = "******" # aqui ingrese su clave segura de 16 digitos de gmail desde el menu clave para aplicaciones
+
+    msg = MIMEText(body, 'plain')  # Usamos 'plain' como tipo de contenido
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+        smtp_server.quit()
+        print("Message sent successfully!")
+    except Exception as e:
+        print(f"Error sending email: {str(e)}")
+
 
 DAG_ID = 'ingesta_carga_redshift'
 DAG_DESCRIPTION = 'Api_rick_and_morti'
@@ -19,28 +43,23 @@ ARGS = {
     'retry_delay': timedelta(minutes=5),
 }
 
+with DAG(dag_id=DAG_ID,
+         description=DAG_DESCRIPTION,
+         default_args=ARGS,
+         schedule_interval=DAG_SCHEDULE,
+         catchup=DAG_CATCHUP,
+         tags=TAGS,
+         max_active_runs=1) as dag:
 
+    name_task_1 = PythonOperator(task_id='job_get_data',
+                                 python_callable=main,
+                                 retries=1,
+                                 retry_delay=timedelta(minutes=1))
 
-with DAG(dag_id = DAG_ID,
-         description = DAG_DESCRIPTION,
-         default_args = ARGS,
-         schedule_interval = DAG_SCHEDULE,
-         catchup = DAG_CATCHUP,
-         tags = TAGS,
-         max_active_runs = 1) as dag:
-    
-    name_task_1 = PythonOperator(task_id = 'job_get_data',
-                                         python_callable = main,
-                                         retries = 1,
-                                         retry_delay = timedelta(minutes=1))
-    
-    name_task_2 = email_operator = EmailOperator(  
-        task_id='send_email',
-        to="o.oaguilera2@gmail.com",  
-        subject="Test Email - Para la entrega final de Coder House 08-09-2023",  
-        html_content=None,  
-        dag=dag)
-
+    name_task_2 = PythonOperator(task_id='send_email_task',
+                                 python_callable=send_email,
+                                 retries=1,
+                                 retry_delay=timedelta(minutes=1))
 
 
 
